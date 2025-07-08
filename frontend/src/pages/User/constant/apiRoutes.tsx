bd5cca82b50ff216dc0b39dde3326dc65a6f4c11
pawnesh <pawneshkitio@gmail.com>
2025-07-08 12:00:49 +0530

    Url Updated

--- frontend/src/pages/User/constant/apiRoutes.tsx
+++ frontend/src/pages/User/constant/apiRoutes.tsx
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:5000';
-// const BASE_URL = 'https://quickcash.oyefin.com';
+// const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://quickcash.oyefin.com';
 
 const getURL = (url: string): string => {
   return BASE_URL + '/api/v1/digital-signature' + url;
